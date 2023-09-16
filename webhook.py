06942c86677e5e15445ab898b17a807f932d0918
Petra Michael <80101051+aimatochysia@users.noreply.github.com>
2023-09-16 20:04:20 +0700

    Update webhook.py

--- webhook.py
+++ webhook.py
@@ -1,38 +1,46 @@
-import json
-import requests
+import os
+from flask import Flask, request, abort
+from linebot import LineBotApi, WebhookHandler
+from linebot.models import MessageEvent, TextSendMessage
 
-LINE_CHANNEL_ID = "2000778950"
-LINE_CHANNEL_SECRET = "56b0fd26c73d11b1d92a56f9a16707d8"
-LINE_ACCESS_TOKEN = "YO8hmjXdzmiEjW/bjXupqRTSM1HfQ4enoT4W9uCn56KDCgGp/zToQm0mb7DCwj0K8NSWoeHXoWS5xKeuLdv32hTns+KnzRTnnzlepnsF4ZT2fhEURnL5JvvufWhAi3opoCGvTU5XeDCobxfpsBcAwgdB04t89/1O/w1cDnyilFU="
+app = Flask(__name__)
 
-def webhook(request):
-  # Get the JSON payload from the request.
-  payload = json.loads(request.body)
+# Replace these with your LINE bot credentials
+LINE_CHANNEL_ACCESS_TOKEN = 'YO8hmjXdzmiEjW/bjXupqRTSM1HfQ4enoT4W9uCn56KDCgGp/zToQm0mb7DCwj0K8NSWoeHXoWS5xKeuLdv32hTns+KnzRTnnzlepnsF4ZT2fhEURnL5JvvufWhAi3opoCGvTU5XeDCobxfpsBcAwgdB04t89/1O/w1cDnyilFU='
+LINE_CHANNEL_SECRET = '56b0fd26c73d11b1d92a56f9a16707d8'
 
-  # Get the user ID of the person to tag.
-  tagged_user_id = payload["events"][0]["source"]["userId"]
+line_bot_api = LineBotApi(LINE_CHANNEL_ACCESS_TOKEN)
+handler = WebhookHandler(LINE_CHANNEL_SECRET)
 
-  # Get the message to send.
-  message = f"Hey <@Jansen W Baru>, Kerjain Genshinnya anjing!"
+# Replace with the user ID or display name of "Jansen W Baru"
+TARGET_USER = "Jansen W Baru"
 
-  # Send the message to the group.
-  requests.post(
-    "https://api.line.me/v2/bot/message/push",
-    headers={
-      "Authorization": "Bearer {}".format(LINE_ACCESS_TOKEN)
-    },
-    json={
-      "to": payload["events"][0]["source"]["groupId"],
-      "messages": [
-        {
-          "type": "text",
-          "text": message
-        }
-      ]
-    }
-  )
+@app.route("/callback", methods=['POST'])
+def callback():
+    signature = request.headers['X-Line-Signature']
+    body = request.get_data(as_text=True)
+    try:
+        handler.handle(body, signature)
+    except Exception as e:
+        print(e)
+        abort(400)
+    return 'OK'
 
-  # Return a response.
-  return json.dumps({
-    "message": "Webhook processed successfully."
-  })
+@handler.add(MessageEvent, message=None)
+def handle_message(event):
+    if event.source.type == 'group':
+        group_id = event.source.group_id
+        user_id = event.source.user_id
+        message_text = event.message.text
+
+        # Check if the message is from the correct group and sender
+        if message_text.lower() == 'send daily message' and user_id == TARGET_USER:
+            # Replace with your daily message content
+            daily_message = "This is your daily message."
+
+            # Send the message back to the group
+            line_bot_api.push_message(group_id, TextSendMessage(text=daily_message))
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
