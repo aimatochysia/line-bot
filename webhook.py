8cc7814ad8dda55f50747f39f63d4e2d437e677a
Petra Michael <80101051+aimatochysia@users.noreply.github.com>
2023-09-16 16:31:56 +0700

    Update webhook.py

--- webhook.py
+++ webhook.py
@@ -1,6 +1,10 @@
 import json
 import requests
 
+LINE_CHANNEL_ID = "2000778950"
+LINE_CHANNEL_SECRET = "56b0fd26c73d11b1d92a56f9a16707d8"
+LINE_ACCESS_TOKEN = "YO8hmjXdzmiEjW/bjXupqRTSM1HfQ4enoT4W9uCn56KDCgGp/zToQm0mb7DCwj0K8NSWoeHXoWS5xKeuLdv32hTns+KnzRTnnzlepnsF4ZT2fhEURnL5JvvufWhAi3opoCGvTU5XeDCobxfpsBcAwgdB04t89/1O/w1cDnyilFU="
+
 def webhook(request):
   # Get the JSON payload from the request.
   payload = json.loads(request.body)
@@ -9,13 +13,13 @@ def webhook(request):
   tagged_user_id = payload["events"][0]["source"]["userId"]
 
   # Get the message to send.
-  message = f"Hey <@{tagged_user_id}>, this is your daily reminder!"
+  message = f"Hey <@Jansen W Baru>, Kerjain Genshinnya anjing!"
 
   # Send the message to the group.
   requests.post(
     "https://api.line.me/v2/bot/message/push",
     headers={
-      "Authorization": "Bearer YO8hmjXdzmiEjW/bjXupqRTSM1HfQ4enoT4W9uCn56KDCgGp/zToQm0mb7DCwj0K8NSWoeHXoWS5xKeuLdv32hTns+KnzRTnnzlepnsF4ZT2fhEURnL5JvvufWhAi3opoCGvTU5XeDCobxfpsBcAwgdB04t89/1O/w1cDnyilFU="
+      "Authorization": "Bearer {}".format(LINE_ACCESS_TOKEN)
     },
     json={
       "to": payload["events"][0]["source"]["groupId"],
