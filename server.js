e524f8c367122caf09b3b379eb3ff056d92540da
Petra Michael <petraxmichael@gmail.com>
2024-07-20 20:48:41 +0700

    v0..4 dynamic input update

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const axios = require('axios');
 const { v4: uuidv4 } = require('uuid');
 require('dotenv').config();
+
 const authUsername = process.env.HTTP_AUTH_USERNAME;
 const authPassword = process.env.HTTP_AUTH_PASSWORD;
 
@@ -12,27 +13,24 @@ module.exports = async (req, res) => {
             const credentials = Buffer.from(base64Credentials, 'base64').toString('ascii');
             const [username, password] = credentials.split(':');
             if (username === authUsername && password === authPassword) {
-                const url = 'https://api.line.me/v2/bot/message/push';
-                const headers = {
-                    'Content-Type': 'application/json',
-                    'Authorization': `Bearer ${process.env.LINE_ACCESS_TOKEN}`,
-                    'X-Line-Retry-Key': uuidv4()
-                };
-                const body = {
-                    to: process.env.LINE_TO,
-                    messages: [
-                        {
-                            type: 'text',
-                            text: 'JANSEN KERJAIN GENSHIT PETRA'
-                        },
-                        {
-                            type: 'text',
-                            text: 'DING DING DING'
-                        }
-                    ]
-                };
-
                 try {
+                    const { messages } = req.body;
+
+                    if (!messages || !Array.isArray(messages) || messages.length === 0) {
+                        return res.status(400).send('Bad Request: Invalid message format');
+                    }
+
+                    const url = 'https://api.line.me/v2/bot/message/push';
+                    const headers = {
+                        'Content-Type': 'application/json',
+                        'Authorization': `Bearer ${process.env.LINE_ACCESS_TOKEN}`,
+                        'X-Line-Retry-Key': uuidv4()
+                    };
+                    const body = {
+                        to: process.env.LINE_TO,
+                        messages: messages.map(text => ({ type: 'text', text }))
+                    };
+
                     const response = await axios.post(url, body, { headers });
                     console.log('Message sent:', response.data);
                     return res.status(200).send('Message sent successfully');
